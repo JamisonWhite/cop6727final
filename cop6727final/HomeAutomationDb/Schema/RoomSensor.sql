759e4ce211fd4b3150c385988940af192aa06121
Jamie White <robertjamisonwhite@gmail.com>
2015-04-07 22:26:12 -0500

    missed a file

---
+++ cop6727final/HomeAutomationDb/Schema/RoomSensor.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [dbo].[RoomSensor]
+(
+	[RoomName] NVARCHAR(50) NOT NULL , 
+    [SensorDate] DATETIME NOT NULL, 
+    [Occupied] BIT NOT NULL, 
+    [Temperature] DECIMAL NOT NULL, 
+    PRIMARY KEY ([SensorDate], [RoomName]), 
+    CONSTRAINT [FK_RoomSensor_Room] FOREIGN KEY ([RoomName]) REFERENCES [Room]([Name])
+)
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_RoomSensor]
+    ON [dbo].[RoomSensor]
+    FOR INSERT, UPDATE
+    AS
+    BEGIN
+        SET NoCount ON
+
+		--TODO Update Room.TargetTemperature
+
+    END
