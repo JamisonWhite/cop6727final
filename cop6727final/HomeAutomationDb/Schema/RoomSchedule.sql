8053d2f34cabefe30085e0f3449a954cd69870f7
Jamie White <robertjamisonwhite@gmail.com>
2015-04-07 22:25:57 -0500

    added triggers, but not logic...

---
+++ cop6727final/HomeAutomationDb/Schema/RoomSchedule.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE [dbo].[RoomSchedule]
+(
+	[RoomName] NVARCHAR(50) NOT NULL , 
+    [DayOfWeek] SMALLINT NOT NULL, 
+    [StartTime] TIME NOT NULL, 
+    [EndTime] TIME NOT NULL, 
+    [OccupiedTemperature] DECIMAL NOT NULL, 
+    [UnoccupiedTemperature] DECIMAL NOT NULL, 
+    PRIMARY KEY ([RoomName], [StartTime], [DayOfWeek]), 
+    CONSTRAINT [CK_RoomSchedule_EndTime] CHECK (EndTime > StartTime), 
+    CONSTRAINT [FK_RoomSchedule_Room] FOREIGN KEY ([RoomName]) REFERENCES [Room]([Name])
+)
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_RoomSchedule]
+    ON [dbo].[RoomSchedule]
+    FOR INSERT, UPDATE
+    AS
+    BEGIN
+        SET NoCount ON
+
+		-- TODO CONFIRM THERE ARE NO OVERLAPPING SCHEDULES
+
+    END
