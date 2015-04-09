ec56440892414cc485d1e4c7e2989e1ec274d32e
Jamie White <robertjamisonwhite@gmail.com>
2015-04-08 22:02:06 -0500

    added room sensor trigger and sample data

--- cop6727final/HomeAutomationDb/Schema/Room.sql
+++ cop6727final/HomeAutomationDb/Schema/Room.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[Room]
 (
     [Id] INT NOT NULL, 
-	[Name] NVARCHAR(50) NOT NULL , 
-    [BuildingId] INT NOT NULL, 
-    [Occupied] BIT NOT NULL, 
+	[Name] NVARCHAR(50) NOT NULL ,
+    [BuildingId] INT NOT NULL,  
+    [DefaultTemperature] DECIMAL NOT NULL, 
+    [Occupied] BIT NOT NULL DEFAULT 0, 
     [Temperature] DECIMAL NULL, 
     [TargetTemperature] DECIMAL NULL, 
     PRIMARY KEY ([Id]), 
--- cop6727final/HomeAutomationDb/Schema/RoomSensor.sql
+++ cop6727final/HomeAutomationDb/Schema/RoomSensor.sql
@@ -19,4 +19,25 @@ CREATE TRIGGER [dbo].[Trigger_RoomSensor]
 
 		--TODO Update Room.TargetTemperature
 
+		UPDATE Room SET 
+		Occupied = i.Occupied,
+		Temperature = i.Temperature,
+		TargetTemperature = ISNULL( case when i.Occupied = 1 then s.OccupiedTemperature else s.UnoccupiedTemperature end, Room.DefaultTemperature)
+		FROM inserted as i
+		LEFT JOIN RoomSchedule AS s ON 
+				i.RoomId = s.RoomId 
+			and cast(i.SensorDate as Time) between s.StartTime and s.EndTime
+			and DATEPART(DW, i.SensorDate) = s.DayOfWeek
+		WHERE Room.Id = i.RoomId
+
+
+
+
+
     END
+GO
+
+
+DENY UPDATE, DELETE ON RoomSensor TO public
+GO
+
